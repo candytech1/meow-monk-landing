b1292ecad8cfd40fa9c6e9920d6712d03043e237
candytech1 <candybusapp@gmail.com>
2025-11-26 21:30:09 +0700

    Fix routing: Change BrowserRouter to HashRouter for GitHub Pages compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import MeowMonk from './MeowMonk';
 import Stickers from './Stickers';
 import './index.css';
